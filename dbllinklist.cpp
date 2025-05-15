4717a93c0f4e9ba8255389d4b3fa3ef61ee0ef07
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-15 15:45:42 +0700

    step 9d

--- dbllinklist.cpp
+++ dbllinklist.cpp
@@ -75,6 +75,6 @@ public:
         //insert last node
         if(current->next != NULL)
         current->next->prev = newNode; //step 9c : current.next.prev = newNode
-            
+            current->next = newNode; // step 9d : current.next = newbie
     }
 };
